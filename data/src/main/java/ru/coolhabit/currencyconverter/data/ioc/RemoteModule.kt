17a44cbf2e9f2200e27a73ac826de16073f3a6cb
Coolhabit <coolhabit92@gmail.com>
2022-08-18 11:19:49 +0300

    much refactoring

--- data/src/main/java/ru/coolhabit/currencyconverter/data/ioc/RemoteModule.kt
+++ data/src/main/java/ru/coolhabit/currencyconverter/data/ioc/RemoteModule.kt
@@ -2,16 +2,12 @@ package ru.coolhabit.currencyconverter.data.ioc
 
 import dagger.Module
 import dagger.Provides
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import ru.coolhabit.currencyconverter.data.network.CurrencyApi
-import ru.coolhabit.currencyconverter.data.utils.API
 import ru.coolhabit.currencyconverter.data.utils.ApiConstants
 import javax.inject.Singleton
-
-private const val APIKEY = "app_id"
 
 @Module
 class RemoteModule {
@@ -20,9 +16,7 @@ class RemoteModule {
     fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
         .addInterceptor { chain ->
             val request = chain.request()
-            val url = request.url.newBuilder()
-//                .addQueryParameter(APIKEY, API.API_KEY)
-                .build()
+            val url = request.url.newBuilder().build()
             val resultRequest = request.newBuilder().url(url).build()
             chain.proceed(resultRequest)
         }
@@ -40,4 +34,3 @@ class RemoteModule {
     @Singleton
     fun provideCurrencyApi(retrofit: Retrofit): CurrencyApi = retrofit.create(CurrencyApi::class.java)
 }
-
