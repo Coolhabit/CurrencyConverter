972cd3eab19874162b2849ca65e42b5bdccdd68e
Coolhabit <coolhabit92@gmail.com>
2022-08-18 10:54:39 +0300

    all except filters

---
+++ domain/src/main/java/ru/coolhabit/currencyconverter/extensions/CurrencyExt.kt
@@ -0,0 +1,7 @@
+package ru.coolhabit.currencyconverter.extensions
+
+import ru.coolhabit.currencyconverter.entities.dto.Currency
+
+fun List<Currency>.toSymbols(): String {
+    return this.joinToString(",") { it.currencyName }
+}
